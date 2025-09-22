30f856b55c26b4d897272618642eeada4f42968c
kl <klstr999@gmail.com>
2025-09-22 20:50:05 +0500

    Add get key, set key, new error handling?

--- cds/src/cds.rs
+++ cds/src/cds.rs
@@ -1,21 +1,36 @@
-use std::error::Error;
+use std::{
+    collections::HashMap, sync::{Arc, Mutex}
+};
 
 pub struct Cds {
-
+    collection: Arc<Mutex<HashMap<String, String>>>,
 }
 
 impl Cds {
-    pub fn new() -> Result<Cds, Box<dyn Error>> {
-        Ok(Cds {})
+    pub fn new() -> Result<Cds, String> {
+        Ok(Cds {
+            collection: Arc::new(Mutex::new(HashMap::new())),
+        })
     }
-    
-    pub fn get_key(&self, key: String) -> Result<String, Box<dyn Error>> {
-        // Example of creating a dynamic error
-        Err(format!("Not implemented").into())
+
+    pub fn get_key(&self, key: String) -> Result<Option<String>, String> {
+        let collection = self.collection.lock()
+            .map_err(|x| format!("col lock!\n{}", x))?;
+        let val = collection.get(&key);
+
+        if let Some(val) = val {
+            return Ok(Some(val.to_string()));
+        }
+
+        Ok(None)
     }
-    
-    pub fn set_key(&self, key: String, val: String) -> Result<(), Box<dyn Error>> {
-        // Another example
-        Err("Storage not implemented yet".into())
+
+    pub fn set_key(&mut self, key: String, val: String) -> Result<(), String> {
+        let mut collection = self.collection.lock()
+            .map_err(|x| format!("col lock!\n{}", x))?;
+        
+        collection.insert(key, val);
+
+        Ok(())
     }
 }
--- cds/src/main.rs
+++ cds/src/main.rs
@@ -1,14 +1,13 @@
 use crate::cds::Cds;
-use std::error::Error;
 
 mod cds;
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<(), String> {
     let mut cds = Cds::new()?;
 
     let val = cds.get_key("A".to_string())?;
 
-    println!("key 'A' = {}", val);
+    println!("key 'A' = {:?}", val);
 
     cds.set_key("A".to_string(), "Hello".to_string())?;
     
