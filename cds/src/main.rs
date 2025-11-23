a30bc63b2c90e7aade652f2c555fd32be171c08f
kl999 <klstr999@gmail.com>
2025-11-23 09:58:04 +0000

    Add main remote

--- cds/src/main.rs
+++ cds/src/main.rs
@@ -1,4 +1,4 @@
-use crate::cds::Cds;
+use crate::{cds::Cds, cds_worker::PeerMapItem};
 
 mod cds;
 mod peer;
@@ -6,7 +6,8 @@ mod kv_message;
 mod cds_worker;
 
 fn main() -> Result<(), String> {
-    let cds = Cds::new(1, "127.0.0.1:3000".to_string(), vec![])?;
+    let remote = PeerMapItem::new("localhost:3001".to_string(), 2);
+    let cds = Cds::new(1, "127.0.0.1:3000".to_string(), vec![remote])?;
 
     let val = cds.get_key("A".to_string())?;
 
